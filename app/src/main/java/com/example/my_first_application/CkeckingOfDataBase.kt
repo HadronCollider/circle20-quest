ead5fc85813a1ba9bd375c55a155df499ba778bb
Sasha <sanek23072004@gmail.com>
2021-01-25 17:24:19 +0300

    data base fix

--- app/src/main/java/com/example/my_first_application/CkeckingOfDataBase.kt
+++ app/src/main/java/com/example/my_first_application/CkeckingOfDataBase.kt
@@ -1,37 +1,93 @@
 package com.example.my_first_application
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import androidx.fragment.app.FragmentActivity
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import android.content.ContentValues.TAG
+import kotlinx.android.synthetic.main.activity_ckecking_of_data_base.*
 
 
 class CkeckingOfDataBase : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ckecking_of_data_base)
+        //val database = FirebaseDatabase.getInstance()
+        //val myRef = database.getReference("test")
 
-        val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("test")
-        var textOf =findViewById<TextView>(R.id.editTextTextPersonName)
-        var text=textOf.getText().toString()
-        myRef.setValue(text)
-        myRef.addValueEventListener(object :ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val value = dataSnapshot.getValue()
-                Log.d("Tag", "value is: $value")
-                var name=findViewById<TextView>(R.id.textView3)
-                name.text="${value}"
-            }
-            override fun  onCancelled(error: DatabaseError) {
-                Log.w("tag", "Failed to read value.", error.toException())
 
-            }
-            })
+        //button9.setOnClickListener {
+            //var textOf =findViewById<TextView>(R.id.editTextTextPersonName)
+            //var text=textOf.getText().toString()
+            //myRef.setValue(text)
+        //}
+
+
+        //myRef.addValueEventListener(object : ValueEventListener {
+            //override fun onDataChange(dataSnapshot: DataSnapshot) {
+                //val value = dataSnapshot.getValue()
+               // Log.d("Tag", "value is: $value")
+               // var name = findViewById<TextView>(R.id.textView3)
+                //name.text = "${value}"
+            //}
+
+            //override fun onCancelled(error: DatabaseError) {
+                //Log.w("tag", "Failed to read value.", error.toException())
+
+            //}
+        //})
+
+        val database2 = FirebaseFirestore.getInstance()
+        val storage = Firebase.storage
+        val storageRef = storage.reference
+        val db = Firebase.firestore
+
+        val testNote = hashMapOf(
+                "text" to "note"
+        )
+        //val test1="string"
+
+// Add a new document with a generated ID
+        db.collection("testing")
+                .add(testNote)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                }
+        val getting=db.collection("testing").get()
+        var name = findViewById<TextView>(R.id.textView3)
+        name.text = "${getting}"
+
+
+
+        db.collection("testing")
+                .get()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        for (document in task.result) {
+                            Log.d(TAG, document.id + " => " + document.data)
+                        }
+                    } else {
+                        Log.w(TAG, "Error getting documents.", task.exception)
+                    }
+                }
+        button9.setOnClickListener {
+            var textOf =findViewById<TextView>(R.id.editTextTextPersonName)
+            var text=textOf.getText().toString()
+            val TestText = hashMapOf(
+                    "text" to text
+            )
+            db.collection("testing")
+                    .add(TestText)
+        }
 
     }
 }
--- app/src/main/java/com/example/my_first_application/Main_map.kt
+++ app/src/main/java/com/example/my_first_application/Main_map.kt
@@ -4,9 +4,12 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
+import android.os.Parcelable
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -14,14 +17,19 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
+import java.util.logging.Logger.global
+
 
 class Main_map : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationClickListener, GoogleMap.OnMyLocationButtonClickListener {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_map)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -47,18 +55,30 @@ class Main_map : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocation
         map.isMyLocationEnabled = true
         mMap.setMyLocationEnabled(true);
         mMap.setOnMyLocationButtonClickListener(this);
-        mMap.setOnMyLocationClickListener(this);
+        mMap.setOnMyLocationClickListener(this)
+
+        //val loc = fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? -> }
+        //Toast.makeText(this, loc.distanceTo)
 
     }
 
 
 
     override fun onMyLocationClick(location: Location) {
-        Toast.makeText(this, "Current location:\n$location", Toast.LENGTH_LONG).show()
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            return
+        }
+        val SPB = LatLng(59.57, 30.19)
+        val loc = fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? -> }
+        //public var dist =  distanceTo(loc, SPB)
+        //public var dist2 = distanceBetween(SPB, loc):Unit
+        Toast.makeText(this, "Current location:\n${0}", Toast.LENGTH_LONG).show()
     }
 
     override fun onMyLocationButtonClick(): Boolean {
-        val SPB = LatLng(59.57, 30.19)
+
         Toast.makeText(this, "Rasstoyanie is 8", Toast.LENGTH_LONG).show()
         return false
     }
